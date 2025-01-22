962ffb936829bac8c51a7f475eeb187b73994c50
wooyounglee <wooyounglee3312@naver.com>
2025-01-22 19:26:17 +0900

    user info update method

--- src/main/java/com/example/instant_inting/service/UserService.java
+++ src/main/java/com/example/instant_inting/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.instant_inting.service;
 
 import com.example.instant_inting.domain.User;
+import com.example.instant_inting.dto.InstarIdUpdateDto;
 import com.example.instant_inting.dto.JoinDto;
+import com.example.instant_inting.dto.PwUpdateDto;
+import com.example.instant_inting.dto.UserIdUpdateDto;
 import com.example.instant_inting.exception.NotFoundUserException;
 import com.example.instant_inting.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -40,5 +43,32 @@ public class UserService {
             throw new NotFoundUserException(id);
         }
         userRepository.deleteById(id);
+    }
+
+    // pw 변경
+    public void updatePw(Long id, PwUpdateDto pwUpdateDto){
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new NotFoundUserException(id));
+
+        user.setPassword(pwUpdateDto.getNewPw());
+        userRepository.save(user);
+    }
+
+    // userId 변경
+    public void updateUserId(Long id, UserIdUpdateDto userIdUpdateDto){
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new NotFoundUserException(id));
+
+        user.setUserId(userIdUpdateDto.getNewUserId());
+        userRepository.save(user);
+    }
+
+    // instarId 변경
+    public void updateInstarId(Long id, InstarIdUpdateDto instarIdUpdateDto){
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new NotFoundUserException(id));
+
+        user.setInstarId(instarIdUpdateDto.getNewInstarId());
+        userRepository.save(user);
     }
 }
